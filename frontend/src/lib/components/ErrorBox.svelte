4336d227edec7547e4fc4be20c82282df9395516
jessielw <jessielw4049@gmail.com>
2026-01-23 21:56:53 -0500

    feat: build error box

---
+++ frontend/src/lib/components/ErrorBox.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let error: string = "";
+</script>
+
+{#if error}
+  <div
+    class="mb-6 p-4 bg-red-900/50 border border-red-700 rounded-lg text-red-200"
+  >
+    {error}
+  </div>
+{/if}
