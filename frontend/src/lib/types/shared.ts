a5b6c54471be37475a1f01eae37a3cbdc2908755
jessielw <jessielw4049@gmail.com>
2026-02-01 10:23:56 -0500

    feat: update

--- frontend/src/lib/types/shared.ts
+++ frontend/src/lib/types/shared.ts
@@ -10,3 +10,24 @@ export interface UserProfile extends User {
   avatar_url: string | null;
   created_at: string;
 }
+
+export enum MediaType {
+  Movie = "movie",
+  Series = "series",
+}
+
+export enum ServiceType {
+  Jellyfin = "jellyfin",
+  Plex = "plex",
+  Radarr = "radarr",
+  Sonarr = "sonarr",
+  Seerr = "seerr",
+  General = "general",
+  Notifications = "notifications",
+}
+
+export type LibraryType = {
+  serviceType: ServiceType;
+  mediaType: MediaType;
+  libName: string;
+};
