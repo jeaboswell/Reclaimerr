02c93f7401b16353b6490fda635777f8bb28293a
jessielw <jessielw4049@gmail.com>
2026-02-27 19:47:40 -0500

    feat: media type badge

---
+++ frontend/src/lib/components/requests/MediaTypeBadge.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Badge } from "$lib/components/ui/badge/index.js";
+  import { MediaType } from "$lib/types/shared";
+  import { toTitleCase } from "$lib/utils/strings";
+
+  let { mediaType }: { mediaType: MediaType } = $props();
+</script>
+
+<Badge class={mediaType === MediaType.Movie ? "bg-movie" : "bg-series"}>
+  {toTitleCase(mediaType)}
+</Badge>
