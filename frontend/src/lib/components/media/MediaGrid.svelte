4379380733a08e58275fd30953e90ab2e236fc17
jessielw <jessielw4049@gmail.com>
2026-03-01 19:27:59 -0500

    refactor: less bottom margin

--- frontend/src/lib/components/media/MediaGrid.svelte
+++ frontend/src/lib/components/media/MediaGrid.svelte
@@ -45,7 +45,7 @@
   {:else}
     <!-- grid -->
     <div
-      class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 mb-8"
+      class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 mb-4"
     >
       {#each data.items as media (media.id)}
         <MediaCard {media} {mediaType} {onRequestException} {onViewDetails} />
